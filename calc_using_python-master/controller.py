f633d2aa8ab691734818789b7c5aa45eba74c3e0
namrapatel2511 <21it115@charusat.edu.in>
2023-11-03 13:08:11 +0530

    controller logic

--- calc_using_python-master/controller.py
+++ calc_using_python-master/controller.py
@@ -1,5 +1,5 @@
-# Create a Controller class to connect the GUI and the model
 from functools import partial
+
 ERROR_MSG = 'ERROR'
 
 class Controller:
@@ -8,7 +8,7 @@ class Controller:
         """Controller initializer."""
         self._evaluate = model
         self._view = view
-        # Connect signals and slots
+       
         self._connectSignals()
 
     def _calculateResult(self):
@@ -20,16 +20,56 @@ class Controller:
         """Build expression."""
         if self._view.getDisplayText() == ERROR_MSG:
             self._view.clearDisplay()
-
         expression = self._view.getDisplayText() + sub_exp
         self._view.setDisplayText(expression)
 
     def _connectSignals(self):
         """Connect signals and slots."""
         for btnText, btn in self._view.buttons.items():
-            if btnText not in {'=', 'C'}:
+            if btnText not in {'=', 'C', 'Bin to Dec', 'Dec to Bin', 'Bin to Oct', 'Oct to Bin', 'Bin to Hex', 'Hex to Bin'}:
                 btn.clicked.connect(partial(self._buildExpression, btnText))
-
         self._view.buttons['='].clicked.connect(self._calculateResult)
         self._view.display.returnPressed.connect(self._calculateResult)
         self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+        self._view.buttons['Bin to Dec'].clicked.connect(self._bin_to_dec)
+        self._view.buttons['Dec to Bin'].clicked.connect(self._dec_to_bin)
+        self._view.buttons['Bin to Oct'].clicked.connect(self._bin_to_oct)
+        self._view.buttons['Oct to Bin'].clicked.connect(self._oct_to_bin)
+        self._view.buttons['Bin to Hex'].clicked.connect(self._bin_to_hex)
+        self._view.buttons['Hex to Bin'].clicked.connect(self._hex_to_bin)
+
+    def _bin_to_dec(self):
+        """Convert binary to decimal."""
+        binary = self._view.getDisplayText()
+        decimal = str(int(binary, 2))
+        self._view.setDisplayText(decimal)
+
+    def _dec_to_bin(self):
+        """Convert decimal to binary."""
+        decimal = self._view.getDisplayText()
+        binary = bin(int(decimal))[2:]
+        self._view.setDisplayText(binary)
+
+    def _bin_to_oct(self):
+        """Convert binary to octal."""
+        binary = self._view.getDisplayText()
+        octal = oct(int(binary, 2))[2:]
+        self._view.setDisplayText(octal)
+
+    def _oct_to_bin(self):
+        """Convert octal to binary."""
+        octal = self._view.getDisplayText()
+        binary = bin(int(octal, 8))[2:]
+        self._view.setDisplayText(binary)
+
+    def _bin_to_hex(self):
+        """Convert binary to hexadecimal."""
+        binary = self._view.getDisplayText()
+        hexadecimal = hex(int(binary, 2))[2:]
+        self._view.setDisplayText(hexadecimal)
+
+    def _hex_to_bin(self):
+        """Convert hexadecimal to binary."""
+        hexadecimal = self._view.getDisplayText()
+        binary = bin(int(hexadecimal, 16))[2:]
+        self._view.setDisplayText(binary)
